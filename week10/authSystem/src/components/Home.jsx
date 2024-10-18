4b0eb17eb121f178739b6a082db756c26cc5e472
Shubbu03 <thatcoderguyshubham@gmail.com>
2024-10-18 17:52:23 +0530

    authSystem answer for week10

---
+++ week10/authSystem/src/components/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto mt-8">
+      <h2 className="text-2xl font-bold mb-4">Welcome to the Home Page</h2>
+      <p>This is the main content of your application.</p>
+    </div>
+  );
+};
+
+export default Home;
